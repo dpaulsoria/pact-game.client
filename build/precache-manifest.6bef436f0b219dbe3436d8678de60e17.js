daeaf24fc9da9342fb6542cbbefbe165d37e617c
Paul Soria <dsoria@espol.edu.ec>
2023-01-24 00:58:44 -0500

    build

---
+++ build/precache-manifest.6bef436f0b219dbe3436d8678de60e17.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "a000c571957c03213e4f",
+    "url": "/static/css/main.506f6327.chunk.css"
+  },
+  {
+    "revision": "a000c571957c03213e4f",
+    "url": "/static/js/main.a000c571.chunk.js"
+  },
+  {
+    "revision": "772cdba344a280d55633",
+    "url": "/static/css/1.08e13d47.chunk.css"
+  },
+  {
+    "revision": "772cdba344a280d55633",
+    "url": "/static/js/1.772cdba3.chunk.js"
+  },
+  {
+    "revision": "bcc1755068fbc0a623de",
+    "url": "/static/js/2.bcc17550.chunk.js"
+  },
+  {
+    "revision": "8bdd5ec56eddb75ec2bf",
+    "url": "/static/js/runtime~main.8bdd5ec5.js"
+  },
+  {
+    "revision": "e0713d0516537d314acb71d688c8456d",
+    "url": "/static/media/bgG.e0713d05.png"
+  },
+  {
+    "revision": "e0713d0516537d314acb71d688c8456d",
+    "url": "/static/media/bgY.e0713d05.png"
+  },
+  {
+    "revision": "e0713d0516537d314acb71d688c8456d",
+    "url": "/static/media/bgB.e0713d05.png"
+  },
+  {
+    "revision": "e0713d0516537d314acb71d688c8456d",
+    "url": "/static/media/bgR.e0713d05.png"
+  },
+  {
+    "revision": "a9e20bb96f01878c8fe9d5585bba3cd0",
+    "url": "/static/media/Home.a9e20bb9.png"
+  },
+  {
+    "revision": "4cb08b50eb87ee0c6e3aac51cba99d2f",
+    "url": "/static/media/game-bg-music.4cb08b50.mp3"
+  },
+  {
+    "revision": "4de707d339231730ee135be08f570253",
+    "url": "/static/media/table.4de707d3.jpg"
+  },
+  {
+    "revision": "6b501e3fe3f25dfce85a29c7f7016251",
+    "url": "/static/media/plus2.6b501e3f.png"
+  },
+  {
+    "revision": "df31eb9b96ae70dde0ba2f926162e839",
+    "url": "/static/media/sadface2.df31eb9b.png"
+  },
+  {
+    "revision": "b489709a18599ce68d483f176d3dace6",
+    "url": "/static/media/mas1.b489709a.jpg"
+  },
+  {
+    "revision": "13ba81ee57e8fe8ce86d99c1e5bc1033",
+    "url": "/static/media/mas3.13ba81ee.jpg"
+  },
+  {
+    "revision": "9daa642e23d8ee7b95eea831b57bf4e7",
+    "url": "/static/media/menos1.9daa642e.jpg"
+  },
+  {
+    "revision": "07e37700a21923a30731f73ba984031f",
+    "url": "/static/media/menos3.07e37700.jpg"
+  },
+  {
+    "revision": "295b1fae605602e5432eef49934b418f",
+    "url": "/static/media/pierde.295b1fae.jpg"
+  },
+  {
+    "revision": "5600aacdfa0b2d09654243ff44df7557",
+    "url": "/static/media/reverse.5600aacd.jpg"
+  },
+  {
+    "revision": "83f95975015a0d0ce2f5289efb374000",
+    "url": "/static/media/backCard.83f95975.png"
+  },
+  {
+    "revision": "50eddd01117d02a34c7bbcfd0d653357",
+    "url": "/static/media/logo2.50eddd01.png"
+  },
+  {
+    "revision": "d66fb2dd0818f06089fd0af8bab572e4",
+    "url": "/static/media/logo.d66fb2dd.png"
+  },
+  {
+    "revision": "e0bb8e9e4e9c6001350a2bc510f2c805",
+    "url": "/static/media/quicksand-all-400-normal.e0bb8e9e.woff"
+  },
+  {
+    "revision": "b9377b80c35131e097b04c0775b0aada",
+    "url": "/static/media/quicksand-vietnamese-400-normal.b9377b80.woff2"
+  },
+  {
+    "revision": "0b4895f9d22fc5c0075cd62cf84447d8",
+    "url": "/static/media/quicksand-latin-ext-400-normal.0b4895f9.woff2"
+  },
+  {
+    "revision": "099548fac114f5f6498c5c75b943581d",
+    "url": "/static/media/quicksand-latin-400-normal.099548fa.woff2"
+  },
+  {
+    "revision": "095f50732cdc3ac56aca948149782c0b",
+    "url": "/index.html"
+  }
+];
